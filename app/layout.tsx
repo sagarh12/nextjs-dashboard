1f5144d66a3a2979991a7ff9e20b961ba0c2f4a8
Sagar Hooda <sagarhooda.uscan@gmail.com>
2024-08-01 15:38:05 -0400

    first

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
 
 import '@/app/ui/global.css';
-// Remove the next line as it conflicts with the Google font import
-// import { inter } from '@/app/ui/fonts';
-
 import { Inter } from 'next/font/google';
 
-export const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
@@ -14,7 +11,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className= {'${inter.className} antialiased'}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
+
